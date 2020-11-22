25e538791e624ab34ed16881bc996c3b40068f25
RobotCherries <zdip14@gmail.com>
2020-11-22 23:03:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-basics-lesson-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-basics-lesson-1)
---
+++ index.ts
@@ -0,0 +1,16 @@
+import { Observable, of } from "rxjs";
+import { map } from "rxjs/operators";
+
+const observer = {
+  next: value => console.log("next", value),
+  error: error => console.log("error", error),
+  complete: () => console.log("complete!")
+};
+
+const observable = new Observable(subscriber => {
+  subscriber.next("Hello");
+  subscriber.next("World");
+  subscriber.complete();
+});
+
+observable.subscribe(observer);
